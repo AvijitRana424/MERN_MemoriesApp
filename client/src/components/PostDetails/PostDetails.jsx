f6b14f94f496b625d6082721f1e47bfd83d2b288
AvijitRana424 <avijit1126.be20@chitkarauniversity.edu.in>
2022-10-21 13:01:58 +0530

    update some changes

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -50,7 +50,6 @@ const Post = () => {
           <Typography variant="h6">Created by: {post.name}</Typography>
           <Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
           <Divider style={{ margin: '20px 0' }} />
-          <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
           <Divider style={{ margin: '20px 0' }} />
              
              <CommentSection post={post} />
